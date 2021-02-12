c7b13a65ddf7240b3c6a4c4d54d3cbfab987ddf7
PageKallop <PageMarie78@gmail.com>
2021-02-12 15:29:04 -0600

    Initial Commit

---
+++ CreditCardViewSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreditCardViewSwiftUI
+//
+//  Created by Page Kallop on 2/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
